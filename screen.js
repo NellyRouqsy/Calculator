eb8759808ddbc71def993a81d0ab13a8e7b7d6d6
NellyRouqsy <farouqadedokun@gmail.com>
2024-09-12 08:50:15 +0100

    first push

---
+++ screen.js
@@ -0,0 +1,26 @@
+let screen = document.getElementById("screen")
+
+
+function addNum (number) {
+   screen.value += number
+}
+
+let toCancel = ( )=>{
+   return screen.value = " "
+}
+
+const toDelete = ()=>{
+   screen.value = screen.value.slice(0,-1)
+}
+
+const toMinus = ()=>{
+   screen.value = screen.value * -1
+}
+
+const addPercent = ()=>{
+   screen.value = screen.value**0.5
+}
+
+const toCalculate = ()=>{
+   screen.value = eval(screen.value)
+}
